c7887e6c92b49ccc66a76fc63c5ed9ddb48a8c4c
ShikhaChoubey <smriticool00@gmail.com>
2021-04-04 10:26:23 +0530

    photo listing task in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'photolisting';
+  apiArr: any = [];
+  buttonName = "Compare";
+  config: any;
+
+  tabledata: any =[];
+
+  constructor(
+    private http: HttpClient
+  ){
+    this.config = {
+      itemsPerPage: 10,
+      currentPage: 1
+    }
+  }
+ ngOnInit(){
+   this.http.get('https://jsonplaceholder.typicode.com/photos').subscribe(res=>{
+    //  console.log(res)
+     this.apiArr = res;
+     this.apiArr.forEach(e=>{e['btnName']="Compare"});
+   })
+ }
+
+ onChange(eachdata){
+
+   if(eachdata.btnName==='Compare'){
+     this.tabledata.push(eachdata);
+     console.log(this.tabledata)
+      eachdata.btnName = 'Remove';
+   }else{
+     this.tabledata.splice(
+       this.tabledata.indexOf(eachdata), 1
+     );
+     eachdata.btnName = 'Compare';
+   }
+
+ }
+
+
 }
